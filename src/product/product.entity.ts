5f9081807ae671890a983f33ff871846179462b3
nluit <nluit99@gmail.com>
2020-02-22 11:09:57 +0700

    oke

--- src/product/product.entity.ts
+++ src/product/product.entity.ts
@@ -14,7 +14,7 @@ export class Product  extends BaseEntity {
     img: string;
   @Column('text')
     desc: string;  
-    @Column('number')
+    @Column()
     discount: number; 
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
     public createdAt: number;
